390a29a1b90e8f13f2e9c91cd902d50a8ef16054
jhordy <jhooordy@gmail.com>
2023-10-20 16:07:21 -0300

    chore: create seeds

---
+++ prisma/seed.ts
@@ -0,0 +1,51 @@
+import { faker } from '@faker-js/faker';
+import { db } from '.';
+
+async function seed() {
+  await db.competition.create({
+    data: {
+      name: faker.company.name(),
+      image: faker.image.url(),
+    },
+  });
+
+  await db.competition.create({
+    data: {
+      name: faker.company.name(),
+      image: faker.image.url(),
+    },
+  });
+
+  for (let i = 1; i <= 10; i++) {
+    const team = await db.team.create({
+      data: {
+        name: faker.company.name(),
+        location: faker.location.street(),
+        stadium: faker.location.city(),
+      },
+    });
+
+    for (let j = 1; j <= 11; j++) {
+      await db.player.create({
+        data: {
+          name: faker.person.fullName(),
+          age: faker.number.int({ min: 18, max: 40 }),
+          nationality: faker.location.country(),
+          image: faker.image.avatar(),
+          teamId: team.id,
+        },
+      });
+    }
+  }
+
+  console.log('Seeding completed');
+}
+
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await db.$disconnect();
+  });
